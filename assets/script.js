36cf52a19fb09f9867766272b8a4f1576294f12a
Meow <tabbathapischke@gmail.com>
2023-09-10 15:30:48 -1000

    add local storage display to page function, formatting

--- assets/script.js
+++ assets/script.js
@@ -4,48 +4,45 @@
 $(function () {
   // create const to access Save Btn
   const $saveBtn = $('.saveBtn');
+  const $timeBlocks = $('.time-block');
 
   // Event listener when saving text input, uploads it to local storage
-  $saveBtn.on('click', function() {
+  $saveBtn.on('click', function () {
     const $thisBtn = $(this).parent().attr('id');
     let $hourTextInput = $(this).siblings('.description').val();
 
     localStorage.setItem($thisBtn, $hourTextInput);
   })
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // retrieves saved text from local storage and displays on page
+  $timeBlocks.each(function () {
+    const $thisTimeBlock = $(this);
+    const $timeBlockText = localStorage.getItem($thisTimeBlock.attr('id'));
+
+    $thisTimeBlock.children(".description").text($timeBlockText);
+  })
+
+  // Adds code to display the current date in the header of the page.
+  let $today = dayjs();
+  $('#current-day').text($today.format('dddd, MMMM D, YYYY'));
+
+  // calls function to update color for current time
   updateHour();
 });
 
-  // Adds code to display the current date in the header of the page.
-let $today = dayjs();
-$('#current-day').text($today.format('dddd, MMMM D, YYYY'));
-
 // Updates current hour box color by comparing current hour to hour id number
-  function updateHour() {
-    $('.time-block').each(function() {
-      let hour = dayjs().hour();
-      let $blockHour = parseInt($(this).attr('id').split('-')[1]);
+function updateHour() {
+  $('.time-block').each(function () {
+    let hour = dayjs().hour();
+    let $blockHour = parseInt($(this).attr('id').split('-')[1]);
 
     if ($blockHour < hour) {
-      $(this).addClass('past')
+      $(this).addClass('past');
     } else if ($blockHour === hour) {
-      $(this).addClass('present')
+      $(this).addClass('present');
     } else if ($blockHour > hour) {
-      $(this).addClass('future')
+      $(this).addClass('future');
     }
+  });
+}
 
-    });
-    }
-
- 
